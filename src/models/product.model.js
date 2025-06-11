5264e7695a53f4b872b951352a4a64ebd65aec17
Bermelio <facundoperalta258@gmail.com>
2025-06-11 18:05:25 -0300

    thumbnails add

--- src/models/product.model.js
+++ src/models/product.model.js
@@ -4,6 +4,7 @@ import mongoosePaginate from 'mongoose-paginate-v2';
 const productSchema = new mongoose.Schema({
   title: String,
   description: String,
+  thumbnail: String,
   price: Number,
   stock: Number,
   category: String,
